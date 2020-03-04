b65a008d0bf98e911c5eb7183324da90b3522802
Colin Tong <tongcolin@live.ca>
2020-03-04 00:24:22 -0500

    fix the PR for async handlers

--- src/server/api/oauth/oauth.ts
+++ src/server/api/oauth/oauth.ts
@@ -5,15 +5,20 @@ import { oAuthManager, settingsManager } from 'server/app';
 import { coerceBoolean } from 'server/utils';
 
 router.put('/token', (_req, _res, next) => next());
-router.post('/token', (req, res) => {
-  oAuthManager.setToken(req.body);
-  res.status(204).send();
+router.post('/token', async (req, res) => {
+  try {
+    oAuthManager.setToken(req.body);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
 });
 
 router.get('/token-validity', async (req, res) => {
-  const requestScopes: string | undefined = req.query.scopes;
-  const scopes = requestScopes ? requestScopes.split(' ') : undefined;
   try {
+    const requestScopes: string | undefined = req.query.scopes;
+    const scopes = requestScopes ? requestScopes.split(' ') : undefined;
     res.status(200).send(await oAuthManager.getTokenValidity(scopes));
   } catch (error) {
     console.error(error);
@@ -21,18 +26,28 @@ router.get('/token-validity', async (req, res) => {
   }
 });
 
-router.get('/token-save-status', (_req, res) => {
-  res.status(200).send(!!settingsManager.get('oauth/tokenSaveStatus'));
-});
-router.post('/token-save-status', (req, res) => {
-  const tokenSaveStatus = coerceBoolean(req.body);
-  settingsManager.set('oauth/tokenSaveStatus', tokenSaveStatus);
-  if (tokenSaveStatus) {
-    oAuthManager.setPath(settingsManager.get('oAuthTokenPath'));
-  } else {
-    oAuthManager.deleteTokenSync();
+router.get('/token-save-status', async (_req, res) => {
+  try {
+    res.status(200).send(!!settingsManager.get('oauth/tokenSaveStatus'));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
   }
-  res.status(201).send();
+});
+router.post('/token-save-status', async (req, res) => {
+  try {
+    const tokenSaveStatus = coerceBoolean(req.body);
+    settingsManager.set('oauth/tokenSaveStatus', tokenSaveStatus);
+    if (tokenSaveStatus) {
+      oAuthManager.setPath(settingsManager.get('oAuthTokenPath'));
+    } else {
+      oAuthManager.deleteTokenSync();
+    }
+    res.status(201).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
 });
 
 export default router;
